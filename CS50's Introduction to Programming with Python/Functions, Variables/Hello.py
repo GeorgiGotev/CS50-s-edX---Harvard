9229a435b254e04223d774b671f9757bb4639b28
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2024-03-03 23:08:32 +0200

    print function, many options

---
+++ CS50's Introduction to Programming with Python/Functions, Variables/Hello.py
@@ -0,0 +1,15 @@
+# ask user for their name...
+name = input("What is your name? ")
+
+# say hello to user.
+print(f"Hello, {name}!")
+print("Hello, " + name + "!")
+
+
+print("Hello,",name+"!")
+# change the separator in the basic print
+print("Hello,", name, sep="...")
+
+# combine two lines and print them inline
+print("Hello, ", end="")
+print(name + "!")
