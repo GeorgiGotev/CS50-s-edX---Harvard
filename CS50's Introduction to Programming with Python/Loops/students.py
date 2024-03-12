83dbce3ac5b50e7b9503348140ded2dc02583486
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2024-03-12 23:05:21 +0200

    some example with loops

---
+++ CS50's Introduction to Programming with Python/Loops/students.py
@@ -0,0 +1,11 @@
+students = ["Georgi", "Ivaylo", "Teodora"]
+
+# for i in students:
+#     print("-------")
+#     for n in i:
+#         print(n)
+
+
+for i in range(len(students)):
+    print(f"{i+1} - {students[i]}")
+
