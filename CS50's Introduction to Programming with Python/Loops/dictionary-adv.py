981a7f5eaace5f569d60c8f96e5d742de84a127d
Georgi Gotev <118106429+GeorgiGotev@users.noreply.github.com>
2024-03-13 15:19:21 +0200

    some more functions of dictionaries in python - like dictionary in list with more information

---
+++ CS50's Introduction to Programming with Python/Loops/dictionary-adv.py
@@ -0,0 +1,9 @@
+home = [
+    {"name" : "Georgi", "address" : "Slaveykov", "age" : 31},
+    {"name" : "Teodora", "address" : "M.Rudnik", "age" : 30},
+    {"name" : "Georgi", "address" : "Slaveykov", "age" : 3},
+]
+
+
+for list in home:
+    print(f"{list["name"]} lives in {list["address"]} and is at {list["age"]} old.")
